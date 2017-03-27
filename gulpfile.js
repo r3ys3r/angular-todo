8b5552458ca0c416014cc0acf7c9956f567f4254
Rey Serantes <reyser@gmail.com>
2017-03-27 10:41:27 -0500

    updates to GULPFILE

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,49 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp'),
-	concat = require('gulp-concat');
+// PACKAGES
+var gulp   = require('gulp'),
+	concat = require('gulp-concat'),
+	minify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	align  = require('gulp-align');
 
-gulp.task("concatScripts", function(){
-	gulp.src('js')
-	.pipe(concat())
+// CONCAT SCRIPTS TASK
+gulp.task("concatScript", function(){
+	gulp.src([
+		'js/angular.min.js',
+		'js/todo.js'
+	])
+	.pipe(concat('app.js'))
+	.pipe(gulp.dest("js"));
 });
+
+// MINIFY SCRIPTS TASK
+gulp.task("minifyScripts", function(){
+	gulp.src('js/app.js')
+	.pipe(minify())
+	.pipe(rename('app.min.js'))
+	.pipe(gulp.dest('js'));
+});
+
+// WATCH TASK
+gulp.task('watchFiles', function() {
+  gulp.watch('scss/**/*.scss', ['compileSass']);
+  gulp.watch('js/main.js', ['concatScripts']);
+})
+
+
+// PRODUCTION BUILD TASK
+gulp.task("build", ['concatScript', 'minifyScripts'], function(){
+	return gulp.src([
+		"css/bootstrap.css",
+		"css/todo.css",
+		"js/app.min.js",
+		"fonts/**"
+	],
+	{base: './'}
+	.pipe(gulp.dest('dist'))
+	)	
+});
+
+
+
